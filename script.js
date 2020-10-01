70dcca62a99f71582f3d079795c3409d965a290d
Kelly Danger <kellyanndanger@gmail.com>
2020-10-01 14:33:17 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('hello from js');
+
+//event #2
+$(document).ready(onReady);//the html isn't technically ready until all the events called in the js (see hello() and goodbye() below load because that's sourced in html...so the html isn't loaded until that is complete.)
+
+function onReady(){//does a lot of setup
+  console.log('hello from jq')
+  //add event listeners aka click events
+  
+  //get data to display on the page
+};
+
+function hello(){
+  console.log('hello')
+};
+function goodbye(){
+  console.log('goodbye')
+};
+
+//event #1
+hello();
+goodbye();
+hello();
