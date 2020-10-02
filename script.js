103ffe572a3b18fe89810973303ff45002ecab10
Kelly Danger <kellyanndanger@gmail.com>
2020-10-02 09:38:55 -0500

    continued jQuery lecture

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 console.log('hello from js');
 
-//event #2
+//EVENT #2
 $(document).ready(onReady);//the html isn't technically ready until all the events called in the js (see hello() and goodbye() below load because that's sourced in html...so the html isn't loaded until that is complete.)
+
+let favFoods = ['sushi', 'cheese', 'coffee'];
+
 
 function onReady(){//does a lot of setup
   console.log('hello from jq')
   //add event listeners aka click events
-
   //get data to display on the page
   //$() <---this grabs an element in JQuery
-  $()
+
+  $('#clickMe').on('click', buttonClicked);//event listener
+  populateList(favFoods);//on load
+
 };
+
+function populateList(array){
+  let ulFoods = $('#favoriteFoods');//this is optional, you don't have to assign the jQuery selector to a variable.
+  for(let i = 0; i < array.length; i++){
+    ulFoods.append(`<li>${array[i]}</li>`);
+  }//end for loop
+}//end populateList
+
+//THE BUTTON CLICK IS EVENT #3
+//write a function called buttonClicked
+function buttonClicked(){
+  console.log("I'm Clicked");
+  
+}//end buttonClicked
 
 function hello(){
   console.log('hello')
